dce3e38faf2a7befa19bdd0cf51a912f6d33d09f
Shrish-MCVTS <Shrish.kuswhaha@mcvts.org>
2026-05-24 22:49:11 -0400

    Added api calling

--- main.cpp
+++ main.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
 #include "httplib.h"
 #include "json.hpp"
+
+using json = nlohmann::json;
+
+int main() {
+
+    httplib::Client cli("http://uselessfacts.jsph.pl/");
+
+    auto res = cli.Get("/api/v2/facts/random");
+
+    if (res && res->status == 200) {
+
+        json data = json::parse(res->body);
+
+        std::cout << "Random Fact" << std::endl;
+        std::cout << data["text"] << std::endl;
+        std::cout << data["source"] << std::endl;
+
+    } else {
+        std::cout << "Failed" << std::endl;
+    }
+
+    return 0;
+}
